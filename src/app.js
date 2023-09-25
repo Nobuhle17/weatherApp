26de557692460c064d0fe8e36bfd58072eb1ab6e
Nobuhle` <shekimonobuhle@gmail.com>
2023-09-25 20:54:49 +0200

    updated the Js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+
+function formatDate(timestamp) {
+    let date = new Date (timestamp);
+    let hours =date.getHours();
+    if (hours<10){
+        hours=`0${hours}`;
+    }
+    let minutes=date.getMinutes();
+    if (minutes < 10){
+        minutes =`0${minutes}`;
+    }
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let day= days[date.getDay()];
+
+
+return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
     console.log(response.data.main);
     let temperatureElement = document.querySelector(".temperature");
@@ -6,7 +25,12 @@ let cityElement = document.querySelector("#City");
 cityElement.innerHTML = response.data.name;
 let descriptionElement=document.querySelector(".condition");
 descriptionElement.innerHTML= response.data.weather[0].description;
-
+let humidityElement=document.querySelector("#Humidity");
+humidityElement.innerHTML=response.data.main.humidity;
+let windElement=document.querySelector("#Wind");
+windElement.innerHTML=response.data.wind.speed;
+let dateElement=document.querySelector("#date");
+dateElement.innerHTML=formatDate(response.data.dt *1000);
 }
 
 
@@ -15,9 +39,14 @@ descriptionElement.innerHTML= response.data.weather[0].description;
 
 
 
+
+
+
+
+
+
+
 let apiKey ="22de9332fb541d849c849404a3205132";
-let apiUrl=` https://api.openweathermap.org/data/2.5/weather?q=Mbombela&appid=${apiKey}&units=metrics`;
-
-
+let apiUrl=` https://api.openweathermap.org/data/2.5/weather?q=Mbombela&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
